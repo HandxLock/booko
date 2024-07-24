476130723f5489a230da34fac889a61718cad694
HandxLock <eothandlock@gmail.com>
2024-07-24 17:15:58 -0400

    borra una view

--- booko-frontend/src/App.jsx
+++ booko-frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import Areadetrabajo from './view/areadetrabajo/Areadetrabajo';
 import Primeranecesidad from './view/primeranecesidad/Primeranecesidad';
 import Recreacion from './view/recreacion/Recreacion';
-import AreadetrabajoFecha from './view/areadetrabajo/AreadetrabajoFecha';
+
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/areatrabajo" element={<Areadetrabajo />} />
         <Route path="/primeranecesidad" element={<Primeranecesidad />} />
         <Route path="/recreacion" element={<Recreacion />} />
-        <Route path="/areadetrabajo/fecha" element={<AreadetrabajoFecha />} />
+
       </Routes>
     </BrowserRouter>
   );
